2a4b8d2f51843e7768b58ca628a7b0189d5bacd9
Fernando Miranda <fermirandamonroy@hotmail.com>
2024-05-01 18:33:00 -0600

    Creando el modelo

---
+++ src/main/java/com/example/model/Persona.java
@@ -0,0 +1,18 @@
+package com.example.model;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Builder
+@Getter
+@Setter
+public class Persona {
+	
+	private Integer id;
+	
+	private String name;
+	
+	private String lastName;
+
+}
